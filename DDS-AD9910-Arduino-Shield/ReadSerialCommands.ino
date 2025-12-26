4abd083e739d8c1563f7841ccd963f1ad3ff906d
Jayabrota Banerjee <163514317+jayabrotabanerjee@users.noreply.github.com>
2025-12-26 15:00:04 +0530

    Refactor ReadSerialCommands and update command handling
    Add W for writing directly to profile0

--- DDS-AD9910-Arduino-Shield/ReadSerialCommands.ino
+++ DDS-AD9910-Arduino-Shield/ReadSerialCommands.ino
@@ -1,111 +1,58 @@
+/* FILE: ReadSerialCommands.ino */
+#include "main.h"
+#include "ad9910.h"
+#include "menuclk.h" // Needed to see Ref_Clk
+#include <GParser.h>
 
-#define SERIAL_PACKAGE_MAX_LENGTH 20
+#define SERIAL_PACKAGE_MAX_LENGTH 40
 char SerialBuffer[SERIAL_PACKAGE_MAX_LENGTH];
-
-const char HELP_STRING [] PROGMEM = 
-          "F - Set Frequency in Hz (100000 — 600000000)\n"
-          "P - Set Output Power in dBm (-72 — 0 OR -68 — +4, depending on \"DAC current\")\n"
-          "M - Get Model\n"
-          "V - Get Firmware Version\n"
-          "E - Enable Output\n"
-          "D - Disable Output\n"
-          "h - This Help\n"
-          "; - Commands Separator"
-          "\n"
-          "Example:\n"
-          "F100000;P-2\n"
-          "Set Frequency to 100 kHz, and Output Power to -2 dBm.\n"
-          "Any number of commands in any order is allowed.";
 
 void ReadSerialCommands()
 {
   if (!Serial.available()) return;
-  int RcvCounter=0;
-  RcvCounter = Serial.readBytesUntil('\n', SerialBuffer, 110);
+  int RcvCounter = Serial.readBytesUntil('\n', SerialBuffer, 110);
   if (RcvCounter == 0) return;
   SerialBuffer[RcvCounter]='\0';
 
-  int32_t value=0;
+  uint32_t value=0;
   char command;
 
   GParser data(SerialBuffer, ';');
   int commandsCounter = data.split();
-
-    for (int i=0; i < commandsCounter; i++)
+  for (int i=0; i < commandsCounter; i++)
     {
-      sscanf(data[i], "%c%ld", &command, &value);
+      long tempVal;
+      sscanf(data[i], "%c%li", &command, &tempVal);
+      value = (uint32_t)tempVal;
+
       switch (command)
       {
-        case 'F': //RF Frequency
-          if (inRange(value, LOW_FREQ_LIMIT, HIGH_FREQ_LIMIT))
-          {
-            Serial.print(F("Set freq.: "));
-            Serial.println(value);
-            H = value % 1000;
-            value = value / 1000;
-            K = value % 1000;
-            value = value / 1000;
-            M = value % 1000;
-          } else Serial.println("Frequency is OUT OF RANGE (" + String(LOW_FREQ_LIMIT) + " — " + String(HIGH_FREQ_LIMIT) + ")");
-        break;
-
-        case 'P': //Power, dBm
-          int lowPowerLimit, highPowerLimit;
-          if (DACCurrentIndex == 0) 
-          {
-            lowPowerLimit = -72;
-            highPowerLimit = 0;
-          } else
-          {
-            lowPowerLimit = -68;
-            highPowerLimit = 4;
+        case 'F': // Set Freq
+          if (value >= 100000) {
+            Serial.print(F("Set Freq: ")); Serial.println(value);
+            H = value % 1000; value /= 1000;
+            K = value % 1000; value /= 1000;
+            M = value;
+            MakeOut(); 
+            UpdateDisplay();
+            CheckPLL(); // Report Status
           }
-          if (inRange(value, lowPowerLimit, highPowerLimit))
-          {
-            Serial.print(F("Set Power.: "));
-            Serial.println(value);
-            if (DACCurrentIndex == 0) A = -1 * value; else A = -1 * (value - 4);
-          } else Serial.println("Power is OUT OF RANGE (" + String(lowPowerLimit) + " — " + String(highPowerLimit) + ")");
-        break;
-
-        case 'D': 
-          Serial.println(F("Output Disabled"));
-          digitalWrite(DDS_PWR_DWN_PIN, HIGH);
-          isPWR_DWN = true;
-        break;
-
-        case 'E': 
-          Serial.println(F("Output Enabled"));
-          digitalWrite(DDS_PWR_DWN_PIN, LOW);
-          isPWR_DWN = false;
         break;
         
-        case 'V': //Firmware Version request
-          Serial.println(FIRMWAREVERSION);
-          //Serial.println(value);
+        case 'W': // Manual Debug
+           ManualWriteProfile0(value);
         break;
 
-        case 'M': //Model request
-          Serial.println(F("DDS9910 v2.0"));
-          //Serial.println(value);
+        case 'x': Write_CFR_Reg(0x00, value); break; 
+        case 'y': Write_CFR_Reg(0x01, value); break; 
+        case 'z': Write_CFR_Reg(0x02, value); break; 
+        case 'r': Read_Register((uint8_t)value); break; 
+
+        case 'P': // Power
+          int pwr; sscanf(data[i], "%c%d", &command, &pwr);
+          if (DACCurrentIndex == 0) A = -1 * pwr; else A = -1 * (pwr - 4);
+          MakeOut();
         break;
-
-        case 'h': //Model request
-          Serial.println((const __FlashStringHelper *) HELP_STRING);
-        break;
-
-        default:
-        Serial.print(F("Unknown command:"));
-        Serial.println(command);
-        Serial.println((const __FlashStringHelper *) HELP_STRING);
-      } //switch
-    } //for
-
-    MakeOut();
-    UpdateDisplay();
-}
-
-bool inRange(int32_t val, int32_t minimum, int32_t maximum)
-{
-  return ((minimum <= val) && (val <= maximum));
+      } 
+    } 
 }
